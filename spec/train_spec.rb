5f29fd8f9957552f47598be360c5a23e30745f06
Janice Laset-Parkerson & Vaughn Dice <student@epicodus.com>
2015-08-19 10:34:01 -0700

    add specs for eta and city_id on Train

--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -8,4 +8,18 @@ describe(Train) do
       expect(test_train.id()).to(be_an_instance_of(Fixnum))
     end
   end
+
+  describe('#eta') do
+    it('returns the eta of the train') do
+      test_train = Train.new({:id => nil, :eta => Time.new(2015), :city_id => 1})
+      expect(test_train.eta()).to(eq(Time.new(2015)))
+    end
+  end
+
+  describe('#city_id') do
+    it('returns the city_id of the train') do
+      test_train = Train.new({:id => nil, :eta => Time.new(), :city_id => 1})
+      expect(test_train.city_id()).to(eq(1))
+    end
+  end
 end
