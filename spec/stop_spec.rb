e04de79448585675ca301523c0c98ecc9faac9e0
Janice Laset-Parkerson & Vaughn Dice <student@epicodus.com>
2015-08-20 11:48:56 -0700

    refactor stop_spec to to re-use class variables

--- spec/stop_spec.rb
+++ spec/stop_spec.rb
@@ -1,12 +1,15 @@
 require('spec_helper')
 
 describe(Stop) do
+  before do
+    @train = Train.new({:name => 'Budapest Lightning', :id => nil})
+    @city = City.new({:name => 'Budapest', :id => nil})
+    @stop = Stop.new({:train => @train, :city => @city, :eta => Time.new(2015)})
+  end
+
   describe('#eta') do
     it('returns eta') do
-      train = Train.new({:name => 'Budapest Lightning', :id => nil})
-      city = City.new({:name => 'Budapest', :id => nil})
-      stop = Stop.new({:train => train, :city => city, :eta => Time.new(2015)})
-      expect(stop.eta()).to(eq(Time.new(2015)))
+      expect(@stop.eta()).to(eq(Time.new(2015)))
     end
   end
 
@@ -18,31 +21,22 @@ describe(Stop) do
 
   describe('#save') do
     it('saves a stop') do
-      train = Train.new({:name => 'Budapest Lightning', :id => nil})
-      train.save()
-      city = City.new({:name => 'Budapest', :id => nil})
-      city.save()
-      stop = Stop.new({:train => train, :city => city, :eta => Time.new(2015)})
-      stop.save()
-      expect(Stop.all()).to(eq([stop]))
+      @train.save()
+      @city.save()
+      @stop.save()
+      expect(Stop.all()).to(eq([@stop]))
     end
   end
 
   describe('#city') do
     it('returns the city') do
-      train = Train.new({:name => 'Budapest Lightning', :id => nil})
-      city = City.new({:name => 'Budapest', :id => nil})
-      stop = Stop.new({:train => train, :city => city, :eta => Time.new(2015)})
-      expect(stop.city()).to(eq(city))
+      expect(@stop.city()).to(eq(@city))
     end
   end
 
   describe('#train') do
     it('returns the train') do
-      train = Train.new({:name => 'Budapest Lightning', :id => nil})
-      city = City.new({:name => 'Budapest', :id => nil})
-      stop = Stop.new({:train => train, :city => city, :eta => Time.new(2015)})
-      expect(stop.train()).to(eq(train))
+      expect(@stop.train()).to(eq(@train))
     end
   end
 end
