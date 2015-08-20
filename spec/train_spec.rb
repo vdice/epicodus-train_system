0e465c06d3d0fd25583607fd08c72607e3d5d78d
Janice Laset-Parkerson & Vaughn Dice <student@epicodus.com>
2015-08-20 11:55:27 -0700

    refactor train_spec to re-use class variables

--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -1,18 +1,21 @@
 require('spec_helper')
 
 describe(Train) do
+  before do
+    @test_train = Train.new({:name => 'red', :id => nil})
+    @test_train2 = Train.new({:name => 'red', :id => nil})
+  end
+
   describe('#id') do
     it('returns the id of the train') do
-      test_train = Train.new({:name => 'red', :id => nil})
-      test_train.save()
-      expect(test_train.id()).to(be_an_instance_of(Fixnum))
+      @test_train.save()
+      expect(@test_train.id()).to(be_an_instance_of(Fixnum))
     end
   end
 
   describe('#name') do
     it('returns the name of the train') do
-      test_train = Train.new({:name => 'red', :id => nil})
-      expect(test_train.name()).to(eq('red'))
+      expect(@test_train.name()).to(eq('red'))
     end
   end
 
@@ -24,51 +27,43 @@ describe(Train) do
 
   describe('#save') do
     it('saves a train to the list of trains') do
-      test_train = Train.new({:name => 'red', :id => nil})
-      test_train.save()
-      test_train2 = Train.new({:name => 'red', :id => nil})
-      test_train2.save()
-      expect(Train.all()).to(eq([test_train, test_train2]))
+      @test_train.save()
+      @test_train2.save()
+      expect(Train.all()).to(eq([@test_train, @test_train2]))
     end
   end
 
   describe('#update') do
     it('updates the train name') do
-      test_train = Train.new({:name => 'red', :id => nil})
-      test_train.save()
-      test_train.update({:name => 'blue'})
-      expect(test_train.name()).to(eq('blue'))
+      @test_train.save()
+      @test_train.update({:name => 'blue'})
+      expect(@test_train.name()).to(eq('blue'))
     end
     it('updates cities for train') do
-      test_train = Train.new({:name => 'red', :id => nil})
-      test_train.save()
+      @test_train.save()
       city1 = City.new({:name => 'Portland', :id => nil})
       city2 = City.new({:name => 'Boulder', :id => nil})
       city1.save()
       city2.save()
-      test_train.update({:city_ids => [city1.id(), city2.id()]})
-      expect(test_train.cities()).to(eq([city1, city2]))
+      @test_train.update({:city_ids => [city1.id(), city2.id()]})
+      expect(@test_train.cities()).to(eq([city1, city2]))
     end
   end
 
   describe('#delete') do
     it('deletes the train') do
-      test_train = Train.new({:name => 'red', :id => nil})
-      test_train.save()
-      test_train2 = Train.new({:name => 'red', :id => nil})
-      test_train2.save()
-      test_train.delete()
-      expect(Train.all()).to(eq([test_train2]))
+      @test_train.save()
+      @test_train2.save()
+      @test_train.delete()
+      expect(Train.all()).to(eq([@test_train2]))
     end
   end
 
   describe('.find') do
     it('returns a train instance based on id') do
-      test_train = Train.new({:name => 'red', :id => nil})
-      test_train.save()
-      test_train2 = Train.new({:name => 'blue', :id => nil})
-      test_train2.save()
-      expect(Train.find(test_train.id())).to(eq(test_train))
+      @test_train.save()
+      @test_train2.save()
+      expect(Train.find(@test_train.id())).to(eq(@test_train))
     end
   end
 end
