2afbec5e5ca517718c04ec569371cef4b578df71
Janice Laset-Parkerson & Vaughn Dice <student@epicodus.com>
2015-08-20 11:52:45 -0700

    refactor city_spec to re-use class variables

--- spec/city_spec.rb
+++ spec/city_spec.rb
@@ -1,18 +1,21 @@
 require('spec_helper')
 
 describe(City) do
+  before do
+    @city = City.new({:name => 'Portland', :id => nil})
+    @city2 = City.new({:name => 'Portland', :id => nil})
+  end
+
   describe('#name') do
     it('returns the name of the city') do
-      test_city = City.new({:name => 'Portland', :id => nil})
-      expect(test_city.name()).to(eq('Portland'))
+      expect(@city.name()).to(eq('Portland'))
     end
   end
 
   describe('#id') do
     it('returns the id of the city') do
-      test_city = City.new({:name => 'Portland', :id => nil})
-      test_city.save()
-      expect(test_city.id()).to(be_an_instance_of(Fixnum))
+      @city.save()
+      expect(@city.id()).to(be_an_instance_of(Fixnum))
     end
   end
 
@@ -24,45 +27,38 @@ describe(City) do
 
   describe('#save') do
     it('saves a city') do
-      test_city = City.new({:name => 'Portland', :id => nil})
-      test_city.save()
-      expect(City.all()).to(eq([test_city]))
+      @city.save()
+      expect(City.all()).to(eq([@city]))
     end
   end
 
   describe('#==') do
     it('returns true if the names and ids of two cities are the same') do
-      city1 = City.new({:name => 'Portland', :id => nil})
-      city2 = City.new({:name => 'Portland', :id => nil})
-      expect(city1).to(eq(city2))
+      expect(@city).to(eq(@city2))
     end
   end
 
   describe('#delete') do
     it('lets you delete a city') do
-      city1 = City.new({:name => 'Portland', :id => nil})
-      city1.save()
-      city2 = City.new({:name => 'Portland', :id => nil})
-      city2.save()
-      city1.delete()
-      expect(City.all()).to(eq([city2]))
+      @city.save()
+      @city2.save()
+      @city.delete()
+      expect(City.all()).to(eq([@city2]))
     end
   end
 
   describe('#update') do
     it('lets you update a city with a new name') do
-      test_city = City.new({:name => 'Portland', :id => nil})
-      test_city.save()
-      test_city.update({:name => 'Fremont'})
-      expect(test_city.name()).to(eq('Fremont'))
+      @city.save()
+      @city.update({:name => 'Fremont'})
+      expect(@city.name()).to(eq('Fremont'))
     end
   end
 
   describe('.find') do
     it('returns city based on id') do
-      test_city = City.new({:name => 'Portland', :id => nil})
-      test_city.save()
-      expect(City.find(test_city.id())).to(eq(test_city))
+      @city.save()
+      expect(City.find(@city.id())).to(eq(@city))
     end
   end
 end
